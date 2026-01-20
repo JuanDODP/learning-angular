7dd162123c2c24ec21d6f0f8df50e9d16e659361
Juan Dominguez <juan.dominguez.dp@gmail.com>
2026-01-20 17:05:44 -0600

    .environments add

--- src/app/gifs/components/side-menu-options/side-menu-options.ts
+++ src/app/gifs/components/side-menu-options/side-menu-options.ts
@@ -1,9 +1,31 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
+interface MenuOptions {
+  label: string;
+  sublabel: string;
+  route: string;
+  icon: string;
+}
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SideMenuOptions { }
+export class SideMenuOptions {
+  menuOptions:MenuOptions[] = [
+    {
+      label: 'Trending',
+      sublabel: 'Popular Gifs',
+      route: '/dashboard/trending',
+      icon: 'fa-solid fa-chart-line'
+    },
+    {
+      label: 'Buscar',
+      sublabel: 'Buscar Gifs',
+      route: '/dashboard/search',
+      icon: 'fa-solid fa-magnifying-glass'
+    }
+  ];
+ }
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production:true,
+  companyName: 'Gift App',
+  companyName2: 'App',
+  companySlogan: 'Tus gifts favoritos',
+
+
+};
