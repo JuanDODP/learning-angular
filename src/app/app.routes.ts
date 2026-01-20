2aaa4ee53239f732471bdbe352db0af83c575ba7
Juan Dominguez <juan.dominguez.dp@gmail.com>
2026-01-20 14:17:07 -0600

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifs/pages/dashboard-page/dashboard-page'),
+    children: [
+      {
+        path: 'search',
+        loadComponent: () => import('./gifs/pages/search-page/search-page')
+      },
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifs/pages/trending-page/trending-page')
+      },
+    ],
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
