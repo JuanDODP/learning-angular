8bf8fbcf30cbf295538f144f8a638150ec580667
Juan Dominguez <juan.dominguez.dp@gmail.com>
2026-01-21 17:59:39 -0600

    Search with 'rxjs' add

--- src/app/gifs/pages/search-page/search-page.ts
+++ src/app/gifs/pages/search-page/search-page.ts
@@ -1,9 +1,30 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { GifList } from "../../components/gif-list/gif-list";
+import { GifsService } from '../../services/gifs.service';
+import { Gif } from '../../interfaces/gif.interface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifList],
   templateUrl: './search-page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class SearchPage { }
+export default class SearchPage {
+   gifSearch = inject(GifsService)
+   gifs = signal<Gif[]>([])
+  onSearch(term: string) {
+    console.log('Buscando:', term);
+    this.gifSearch.searchGifs(term).subscribe((response) => {
+      // const gifs = response.data.map(gifData => ({
+      //   id: gifData.id,
+      //   title: gifData.title,
+      //   url: gifData.images.fixed_height.url
+      // }));
+      // this.gif.set(gifs);
+      // console.log('RESULTADOS DE LA BUSQUEDA', response);
+      this.gifs.set(response);
+    });
+  }
+
+
+}
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -4,12 +4,13 @@ import { environment } from '@environments/environment.development';
 import type { GiphyResponse } from '../interfaces/giphy.interface';
 import { Gif } from '../interfaces/gif.interface';
 import { GifMapper } from '../mapper/gif.mapper';
+import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class GifsService {
   private http = inject(HttpClient);
   trendingGifs = signal<Gif[]>([]);
-  trendingGifsLoading=signal<boolean>(true);
+  trendingGifsLoading = signal<boolean>(true);
   constructor() {
     this.getGifs();
   }
@@ -23,10 +24,25 @@ export class GifsService {
         rating: 'g'
       }
     }).subscribe((response) => {
-          const gif = GifMapper.toGifList(response.data);
-          this.trendingGifs.set(gif);
-          console.log('LOS GIGS',gif);
+      const gif = GifMapper.toGifList(response.data);
+      this.trendingGifs.set(gif);
+      console.log('LOS GIGS', gif);
 
     });
+  }
+  // ==========================================================================================
+  searchGifs(term: string) {
+    // To be implemented
+    return this.http.get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+      params: {
+        q: term,
+        api_key: environment.apiKey,
+        limit: '250',
+        rating: 'g'
+      }
+    }).pipe(
+      map(({ data }) => data),
+      map((item) => GifMapper.toGifList(item))
+    );
   }
 }
